3d9355ca3185951ca0559adec43dc9e743789fa3
prasanth-raj-sankar <prasanthraj0910@gmail.com>
2024-05-19 18:45:39 +0530

    dom-1

---
+++ script.js
@@ -0,0 +1,25 @@
+// var ele = document.createElement("div");
+
+// ele.setAttribute("class","conatiner");
+// ele.setAttribute("class","row");
+
+// var child =document.createElement("div")
+
+// child.innerHTML = "this is col"
+
+// ele.append(child)
+
+// document.body.append(ele);
+
+
+function foo() {
+    var name = document.getElementById("name").value;
+    var Middle = document.getElementById("Middle").value;
+    var Lastname = document.getElementById("Lastname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+
+    console.log(`name:${name} Middle:${Middle} Lastname:${Lastname} email${email} phone${phone}`)
+
+
+}
